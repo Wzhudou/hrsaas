e6b2b252d9f8d990dc465510d6174186b1c543f3
studyVue <jingwg3@163.com>
2023-11-13 21:49:25 +0800

    显示用户名和头像

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -7,8 +7,10 @@
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
-          <i class="el-icon-caret-bottom" />
+          <img :src="avatar" class="user-avatar">
+          <span class="name">{{ name }}</span>
+          <!-- 图标 -->
+          <i class="el-icon-setting" />
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
           <router-link to="/">
@@ -44,7 +46,9 @@ export default {
   computed: {
     ...mapGetters([
       'sidebar',
-      'avatar'
+      // 引入头像和用户名称
+      'avatar',
+      'name'
     ])
   },
   methods: {
@@ -116,21 +120,24 @@ export default {
 
       .avatar-wrapper {
         margin-top: 5px;
-        position: relative;
-
+        /* position: relative; */
+        display: flex;
+        align-items: center;
         .user-avatar {
           cursor: pointer;
-          width: 40px;
-          height: 40px;
-          border-radius: 10px;
+          width: 30px;
+          height: 30px;
+          border-radius: 50%;
+        }
+        .name {
+          margin-right: 10px;
+          margin-left: 5px;
+          font-size: 16px;
         }
 
-        .el-icon-caret-bottom {
+        .el-icon-setting {
           cursor: pointer;
-          position: absolute;
-          right: -20px;
-          top: 25px;
-          font-size: 12px;
+          font-size: 20px;
         }
       }
     }
