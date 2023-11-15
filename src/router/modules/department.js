7a8861c77658967b8b3762430510c14ae3b3803c
studyVue <jingwg3@163.com>
2023-11-15 22:18:41 +0800

    创建项目业务模块

---
+++ src/router/modules/department.js
@@ -0,0 +1,16 @@
+import Layout from '@/layout'
+export default {
+  // 路由信息
+  path: '/department',
+  component: Layout, // 一级路由
+  children: [{
+    path: '', // 二级路由地址为空时，表示/department显示一级路由 + 二级路由
+    component: () => import('@/views/department'),
+    name: 'department', // 可以用来跳转也可以标记路由
+    meta: {
+      // 路由元信息 =》 存储数据
+      icon: 'tree', // 菜单图标
+      title: '组织' // 菜单标题
+    }
+  }]
+}
