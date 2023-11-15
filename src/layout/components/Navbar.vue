cbe15d542c82dd4176877a0c1df03d5837898951
studyVue <jingwg3@163.com>
2023-11-15 21:48:46 +0800

    完成修改密码的功能

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -22,7 +22,8 @@
           <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
             <el-dropdown-item>项目地址</el-dropdown-item>
           </a>
-          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
+          <!-- 阻止默认事件 -->
+          <a target="_blank" @click.prevent="updatePassword">
             <el-dropdown-item>修改密码</el-dropdown-item>
           </a>
           <!-- native事件的修饰符 =》 注册组件根元素的原生事件 -->
@@ -32,11 +33,30 @@
         </el-dropdown-menu>
       </el-dropdown>
     </div>
+    <!-- sync  接收子组件传过来的事件和值 -->
+    <el-dialog title="修改密码" :visible.sync="showDialog" width="500px" :append-to-body="true" @close="btnCancel">
+      <el-form ref="passForm" :model="passForm" :rules="rules" label-width="120px">
+        <el-form-item label="旧密码" prop="oldPassword">
+          <el-input v-model="passForm.oldPassword" size="small" show-password></el-input>
+        </el-form-item>
+        <el-form-item label="新密码" prop="newPassword">
+          <el-input v-model="passForm.newPassword" size="small" show-password></el-input>
+        </el-form-item>
+        <el-form-item label="重复密码" prop="confirmPassword">
+          <el-input size="small" show-password v-model="passForm.confirmPassword"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" size="mini" @click="btnOk">确认修改</el-button>
+          <el-button size="mini" @click="btnCancel">取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import {updatePassword} from '@/api/user'
 import Breadcrumb from '@/components/Breadcrumb'
 import Hamburger from '@/components/Hamburger'
 
@@ -44,6 +64,35 @@ export default {
   components: {
     Breadcrumb,
     Hamburger
+  },
+  data() {
+    return {
+      showDialog: false, // 控制弹层的显示和隐藏
+      passForm: {
+        oldPassword: '', // 旧密码
+        newPassword: '', // 新密码
+        confirmPassword: '', // 确认密码
+      },
+      rules: {
+        oldPassword: [
+          { required: true, message: '旧密码不能为空', trigger: 'blur'},
+        ], // 旧密码
+        newPassword: [
+          { required: true, message: '新密码不能为空', trigger: 'blur'},
+          { min: 6, max: 16, message: '新密码长度为6-16位之间', trigger: 'blur'}
+        ], // 新密码
+        confirmPassword: [
+          { required: true, message: '重复密码不能为空', trigger: 'blur'},
+          { trigger: 'blur', validator: (rule, value, callback) => {
+            if(this.passForm.newPassword === value) {
+              callback()
+            }else {
+              callback(new Error('重复密码和新密码不一致'))
+            }
+          }}
+        ], // 确认密码
+      }
+    }
   },
   computed: {
     ...mapGetters([
@@ -61,6 +110,25 @@ export default {
       // 调用退出登录的action
       await this.$store.dispatch('user/logout')
       this.$router.push('/login')
+    },
+    updatePassword() {
+      this.showDialog = true
+    },
+    btnOk() {
+      this.$refs.passForm.validate(async isOk => {
+        if(isOk){
+          // 调用接口
+          await updatePassword(this.passForm)
+          this.$message.success('修改密码成功')
+          // 成功了
+          this.btnCancel()
+        }
+      })
+    },
+    btnCancel() {
+      this.$refs.passForm.resetFields() // 重置表单
+      // 关闭弹层
+      this.showDialog = false
     }
   }
 }
