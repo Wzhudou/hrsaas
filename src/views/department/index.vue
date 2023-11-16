1bde545cc234aff1c624ead4eaae93135081c156
studyVue <jingwg3@163.com>
2023-11-16 21:46:12 +0800

    树形组件的应用

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,17 +1,36 @@
 <template>
   <div class="container">
     <div class="app-container">
-      组织架构
+      <!-- 展示树形结构 -->
+      <el-tree default-expand-all :data="depts" :props="defaultProps"></el-tree>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Department'
+  name: 'Department',
+  data() {
+    return {
+      depts: [{
+        name: '缠枝',
+        children: [{
+          name: 'test'
+        }]
+      }], // 数据属性
+      // 
+      defaultProps: {
+        label: 'name', // 表示显示的字段名字
+        children: 'children' // 读取子节点的字段名
+      }
+    }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-
+.app-container {
+  padding: 30px 140px;
+  font-size: 14px;
+}
 </style>
