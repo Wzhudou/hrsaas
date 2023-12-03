578022a03089a5843be572e210911021ad9710ce
studyVue <jingwg3@163.com>
2023-12-03 15:54:34 +0800

    权限管理页面 - 页面展示

--- src/views/employee/components/image-upload.vue
+++ src/views/employee/components/image-upload.vue
@@ -1,69 +1,70 @@
 <template>
-    <el-upload
-        class="avatar-uploader"
-        action=""
-        :show-file-list="false"
-        :before-upload="beforeAvatarUpload"
-        :http-request="uploadImage">
-        <img v-if="value" :src="value" class="avatar">
-        <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-        <!-- 
+  <el-upload
+    class="avatar-uploader"
+    action=""
+    :show-file-list="false"
+    :before-upload="beforeAvatarUpload"
+    :http-request="uploadImage"
+  >
+    <img v-if="value" :src="value" class="avatar">
+    <i v-else class="el-icon-plus avatar-uploader-icon" />
+    <!--
             action 为自动上传 这个项目是手动上传
             show-file-list 是否展示上传列表
          -->
-    </el-upload>
+  </el-upload>
 </template>
 
 <script>
-    import COS from 'cos-js-sdk-v5'
-  export default {
-    props: {
-        value: {
-            type: String,
-            default: ''
-        },
+import COS from 'cos-js-sdk-v5'
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    // 检查头像上传 =》 判断文件的类型和大小 =》返回true（继续上传）或者false（停止上传）
+    beforeAvatarUpload(file) {
+      // 支持格式 JPEG png gif bmp
+      const isJPG = ['image/jpeg', 'image/png', 'image/gif', 'image/bmp'].includes(file.type)
+      const isLt2M = file.size / 1024 / 1024 < 5 // 5M
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPEG PNG GIF BMP 格式!')
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 5MB!')
+      }
+      return isJPG && isLt2M
     },
-    methods: {
-        // 检查头像上传 =》 判断文件的类型和大小 =》返回true（继续上传）或者false（停止上传）
-        beforeAvatarUpload(file) {
-            // 支持格式 JPEG png gif bmp
-            const isJPG = ['image/jpeg', 'image/png', 'image/gif', 'image/bmp'].includes(file.type)
-            const isLt2M = file.size / 1024 / 1024 < 5; // 5M
-            if (!isJPG) {
-                this.$message.error('上传头像图片只能是 JPEG PNG GIF BMP 格式!');
-            }
-            if (!isLt2M) {
-                this.$message.error('上传头像图片大小不能超过 5MB!');
-            }
-            return isJPG && isLt2M;
-        },
-        // 选择图片之后上传
-        uploadImage(params) {
-            // console.log('pppppp', params);  
-            // params.file    
-            const cos = new COS({
-                SecretId: '',
-                SecretKey: ''
-            }) // 完成cos对象的初始化 
-            cos.putObject({
-                Bucket: '', // 存储桶名称 ap-nanjing
-                Region: 'ap-nanjing', // 地域名称
-                Key: params.file.name, // 文件名称
-                StorageClass: 'STANDARD', // 固定值
-                Body: params.file // 文件对象
-            }, (error, data) => {
-                // console.log('dadadada', data);
-                if(data.statusCode === 200 && data.Location) {
-                    // 拿到了腾讯云返回的地址
-                    // 通过input 自定义事件将地址传过去
-                    this.$emit('input', "http://" + data.Location) // 将地址返回
-                } else {
-                    this.$message.error(error.message)
-                }
-            })
+    // 选择图片之后上传
+    uploadImage(params) {
+      // console.log('pppppp', params);
+      // params.file
+      const cos = new COS({
+        SecretId: '',
+        SecretKey: ''
+      }) // 完成cos对象的初始化
+      cos.putObject({
+        Bucket: '', // 存储桶名称 ap-nanjing
+        Region: 'ap-nanjing', // 地域名称
+        Key: params.file.name, // 文件名称
+        StorageClass: 'STANDARD', // 固定值
+        Body: params.file // 文件对象
+      }, (error, data) => {
+        // console.log('dadadada', data);
+        if (data.statusCode === 200 && data.Location) {
+          // 拿到了腾讯云返回的地址
+          // 通过input 自定义事件将地址传过去
+          this.$emit('input', 'http://' + data.Location) // 将地址返回
+        } else {
+          this.$message.error(error.message)
         }
+      })
     }
   }
+}
 </script>
 
 <style>
--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,12 +1,53 @@
 <template>
   <div class="container">
     <div class="app-container">
-      权限管理
+      <el-button type="primary" size="mini" class="btnAdd">添加权限</el-button>
+      <el-table 
+        :data="list"
+        row-key="id"
+        default-expand-all>
+        <el-table-column label="名称" prop="name"></el-table-column>
+        <el-table-column label="标识" prop="code"></el-table-column>
+        <el-table-column label="描述" prop="description"></el-table-column>
+        <el-table-column label="操作">
+          <template v-slot="{ row }">
+            <el-button size="mini" type="text" v-if="row.type === 1">添加</el-button>
+            <el-button size="mini" type="text">编辑</el-button>
+            <el-button size="mini" type="text">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
 <script>
+  import { getPermissionList } from '@/api/permission'
+  import { transListToTreeData } from '@/utils'
+
 export default {
-  name: 'Permission'
+  name: 'Permission',
+  data() {
+    return {
+      list: []
+    }
+  },
+  created () {
+    this.getPermissionList();
+  },
+  methods: {
+    async getPermissionList() {
+      const result = await getPermissionList()
+      this.list = transListToTreeData(result, 0) // 将列表数据转化成树形
+      console.log(';;;;', this.list);
+      
+    }
+  },
 }
 </script>
+<style lang="scss" scoped>
+  .app-container {
+    .btnAdd{
+      margin: 10px;
+    }
+  }
+</style>
